2fe9411efbe480e0f29c18414b2d828fe743a463
gongbugi <rrobi1492@gmail.com>
2025-12-06 21:22:12 +0900

    feat: Cors 설정

--- backend/src/main/java/com/gongBG/myWeb/config/WebConfig.java
+++ backend/src/main/java/com/gongBG/myWeb/config/WebConfig.java
@@ -1,6 +1,7 @@
 package com.gongBG.myWeb.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -11,9 +12,15 @@ public class WebConfig implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new LoginCheckInterceptor())
                 .order(1)
-                .addPathPatterns("/**")
-                .excludePathPatterns(
-                        "/", "/login", "/signup"
-                );
+                .addPathPatterns("/**");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
