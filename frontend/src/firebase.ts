ebf1d3b0f10ac0ee3bc7403af56c757051ba2af5
gongbugi <rrobi1492@gmail.com>
2025-12-08 01:59:46 +0900

    fix: 타입 선언

--- frontend/src/firebase.ts
+++ frontend/src/firebase.ts
@@ -20,4 +20,4 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-const analytics = getAnalytics(app);
+export const analytics = getAnalytics(app);
