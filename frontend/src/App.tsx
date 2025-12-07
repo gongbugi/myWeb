47c4026dd50453b9cf606585164d2a36318e3d13
gongbugi <rrobi1492@gmail.com>
2025-12-07 12:01:08 +0900

    feat: firebase 라이브러리 설치 및 Client SDK 설정

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Router>
+      <Routes>
+        {/* 앞으로 여기에 페이지를 하나씩 추가할 거예요 */}
+        <Route path="/" element={<h1>홈 페이지 (임시)</h1>} />
+        <Route path="/login" element={<h1>로그인 페이지 (임시)</h1>} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
