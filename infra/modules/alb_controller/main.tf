4a93f329246a1b7faa05b926314b5df8d285bd8b
gongbugi <rrobi1492@gmail.com>
2026-06-16 23:37:50 +0900

    fix: ALB Controller Helm 차트 파싱 에러 및 VPC 인식 오류 수정
    - 특수문자가 포함된 어노테이션 주입 시 발생하는 에러를 values 블록으로 리팩토링
    - 파드 구동 시 VPC 자동 인식 실패로 인한 타임아웃 방지를 위해 `vpc_id` 및 `region` 속성 명시적 주입

--- infra/modules/alb_controller/main.tf
+++ infra/modules/alb_controller/main.tf
@@ -1,63 +1,78 @@
 data "http" "lbc_iam_policy" {
-    url = "https://raw.githubusercontent.com/kubernetes-sigs/aws-load-balancer-controller/main/docs/install/iam_policy.json"
+  url = "https://raw.githubusercontent.com/kubernetes-sigs/aws-load-balancer-controller/main/docs/install/iam_policy.json"
 }
 
 resource "aws_iam_policy" "lbc" {
-    name = "${var.project_name}-${var.env}-lbc-policy"
-    policy = data.http.lbc_iam_policy.response_body
+  name   = "${var.project_name}-${var.env}-lbc-policy"
+  policy = data.http.lbc_iam_policy.response_body
 }
 
 resource "aws_iam_role" "lbc" {
-    name = "${var.project_name}-${var.env}-lbc-role"
-    
-    assume_role_policy = jsonencode({
-        Version = "2012-10-17"
-        Statement = [
-        {
-            Action = "sts:AssumeRoleWithWebIdentity"
-            Effect = "Allow"
-            Principal = {
-                Federated = var.oidc_provider_arn
-            }
-            Condition = {
-                StringEquals = {
-                    "${replace(var.oidc_provider_url, "https://", "")}:sub" = "system:serviceaccount:kube-system:aws-load-balancer-controller"
-                }
-            }
+  name = "${var.project_name}-${var.env}-lbc-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Effect = "Allow"
+        Principal = {
+          Federated = var.oidc_provider_arn
         }
-        ]
-    })
+        Condition = {
+          StringEquals = {
+            "${replace(var.oidc_provider_url, "https://", "")}:sub" = "system:serviceaccount:kube-system:aws-load-balancer-controller"
+          }
+        }
+      }
+    ]
+  })
 }
 
 resource "aws_iam_role_policy_attachment" "lbc" {
-    policy_arn = aws_iam_policy.lbc.arn
-    role       = aws_iam_role.lbc.name
+  policy_arn = aws_iam_policy.lbc.arn
+  role       = aws_iam_role.lbc.name
 }
 
 resource "helm_release" "lbc" {
-    name = "aws-load-balancer-controller"
-    repository = "https://aws.github.io/eks-charts"
-    chart = "aws-load-balancer-controller"
-    
-    namespace = "kube-system"
-    
-    set {
-        name = "clusterName"
-        value = var.cluster_name
-    }
-    
-    set {
-        name = "serviceAccount.create"
-        value = true
-    }
+  name       = "aws-load-balancer-controller"
+  repository = "https://aws.github.io/eks-charts"
+  chart      = "aws-load-balancer-controller"
 
-    set {
-        name = "serviceAccount.name"
-        value = "aws-load-balancer-controller"
-    }
+  namespace = "kube-system"
 
-    set {
-        name = "serviceAccount.annotations.eks.\\.amazonaws\\.com/role-arn"
-        value = aws_iam_role.lbc.arn
-    }
+  set {
+    name  = "clusterName"
+    value = var.cluster_name
+  }
+
+  set {
+    name  = "vpcId"
+    value = var.vpc_id
+  }
+
+  set {
+    name  = "region"
+    value = "ap-northeast-2"
+  }
+
+  set {
+    name  = "serviceAccount.create"
+    value = true
+  }
+
+  set {
+    name  = "serviceAccount.name"
+    value = "aws-load-balancer-controller"
+  }
+
+  values = [
+    yamlencode({
+      serviceAccount = {
+        annotations = {
+          "eks.amazonaws.com/role-arn" = aws_iam_role.lbc.arn
+        }
+      }
+    })
+  ]
 }
--- infra/modules/alb_controller/variables.tf
+++ infra/modules/alb_controller/variables.tf
@@ -1,24 +1,29 @@
 variable "project_name" {
-  description   = "프로젝트 이름"
-  type          = string
+  description = "프로젝트 이름"
+  type        = string
 }
 
 variable "env" {
-    description = "환경 (dev/prod)"
-    type        = string
+  description = "환경 (dev/prod)"
+  type        = string
 }
 
 variable "oidc_provider_arn" {
-    description = "oidc arn"
-    type = string
+  description = "oidc arn"
+  type        = string
 }
 
 variable "oidc_provider_url" {
-    description = "oidc url"
-    type = string
+  description = "oidc url"
+  type        = string
 }
 
 variable "cluster_name" {
-    description = "클러스터 이름"
-    type = string
+  description = "클러스터 이름"
+  type        = string
+}
+
+variable "vpc_id" {
+  description = "VPC ID"
+  type        = string
 }
